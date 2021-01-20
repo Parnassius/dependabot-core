556e8d966a4c7a47a7987b688223cc31d01824f0
Philip Harrison <philip@mailharrison.com>
2021-01-20 17:44:58 +0000

    Add npm lockfile updater spec file

---
+++ npm_and_yarn/spec/dependabot/npm_and_yarn/file_updater/npm_lockfile_updater_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "dependabot/npm_and_yarn/file_updater/npm_lockfile_updater"
+
+RSpec.describe Dependabot::NpmAndYarn::FileUpdater::NpmLockfileUpdater do
+end
